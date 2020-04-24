be10880ace83212f30d74a543b5beba1cfeca3ae
unknown <rajasekhar.veerla28@gmail.com>
2020-04-24 13:03:04 +0530

    commigt

--- src/main/java/com/covid19/covid19/controller/Covid19InfoController.java
+++ src/main/java/com/covid19/covid19/controller/Covid19InfoController.java
@@ -9,7 +9,7 @@ import com.covid19.covid19.response.ResponseObj;
 import com.covid19.covid19.service.Covid19InfoService;
 
 @RestController
-@RequestMapping("/covid19-info1")
+@RequestMapping("/covid19-info")
 public class Covid19InfoController {
 	
 	@Autowired
